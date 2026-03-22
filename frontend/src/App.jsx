e0b4aeac7d4c0b62bb1d1a4500582218d0fe5512
Luis Lagunez <A20216349@alumnos.uady.mx>
2026-03-21 19:48:40 -0600

    Merge develop - features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,28 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css'
-import ScrollToTop from './components/ScrollToTop';
-import HomePage from './components/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import ScrollToTop from './components/common/ScrollToTop';
+import HomePage from './pages/store/HomePage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import NotFound from './pages/NotFound';
 
 function App() {
-
   return (
     <>
-      <ScrollToTop/>
+      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        {/* Ruta principal de la tienda */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Rutas de Autenticación */}
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+
+        {/* Ruta para manejo de error 404 */}
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
